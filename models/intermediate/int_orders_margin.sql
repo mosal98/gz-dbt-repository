1e040c6638bd6edf2d85bf512f7fb3ae3eef1c70
Mo Salameh <momo.salameh97@gmail.com>
2023-11-08 10:32:08 +0000

    New Build

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,24 +1,10 @@
-with joined_data as (
-  select
-    s.date_date,
-    s.orders_id,
-    s.products_id,
-    s.revenue,
-    s.quantity,
-    p.purchase_price
-  from {{ ref('stg_raw__sales') }} AS s
-  join {{ ref('stg_raw__product') }} AS p
-  USING (products_id)
-)
-
-select
-  orders_id,
-  date_date,
-  ROUND(SUM(revenue), 1) as revenue,
-  SUM(quantity) as quantity,
-  ROUND(SUM(quantity * CAST(purchase_price AS FLOAT64)), 1) as purchase_cost,
-  ROUND(SUM(revenue - (quantity * CAST(purchase_price AS FLOAT64))), 1) as margin
-from joined_data
-GROUP BY
-orders_id,
-date_date
+SELECT
+	orders_id,
+	date_date,
+	ROUND(SUM(revenue),2) as revenue,
+	ROUND(SUM(quantity),2) as quantity,
+	ROUND(SUM(purchase_cost),2) as purchase_cost,
+	ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id,date_date
+ORDER BY orders_id DESC
